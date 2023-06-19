033431f27ed38254a535556a7d727c3f436a3706
muramatsu <t.muramatsu0485@gmail.com>
2023-06-19 13:19:08 +0900

    Java　ファイル削減

--- demo/src/main/java/com/example/demo/repository/InspectRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.InspectModel;
-
-import java.util.List;
-
-public interface InspectRepository {
-    List<InspectModel> getAll();
-}
---
+++ demo/src/main/java/com/example/demo/service/MainService.java
@@ -0,0 +1,59 @@
+package com.example.demo.service;
+
+import com.example.demo.controller.order.OrderRequest;
+import com.example.demo.model.InspectModel;
+import com.example.demo.model.OrderModel;
+import com.example.demo.model.RegisterModel;
+import com.example.demo.model.StockModel;
+import com.example.demo.repository.MainRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class MainService {
+    private final MainRepository repository;
+
+    public MainService(MainRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<RegisterModel> getAllFood() {
+        return repository.getAllFood();
+    }
+
+    public void registerFood(RegisterModel model) {
+        repository.registerFood(model);
+    }
+
+    public RegisterModel getById(int foodId) {
+        return repository.getById(foodId);
+    }
+
+    public void updateFood(RegisterModel model) {
+        repository.updateFood(model);
+    }
+    public void deleteFood(int foodId) {
+        repository.deleteFood(foodId);
+    }
+
+    public List<OrderModel> getAllOrder() {
+        return repository.getAllOrder();
+    }
+    public void insertOrder(int foodId, OrderRequest request) {
+        repository.insertOrder(foodId, request);
+    }
+    /*public OrderModel getById(int foodId) {
+        return repository.getById(foodId);
+    }
+    public void updateOrder(OrderModel model) {
+        repository.updateOrder(model);
+    }*/
+
+    public List<StockModel> getAllStock() {
+        return repository.getAllStock();
+    }
+
+    public List<InspectModel> getAllInspect() {
+        return repository.getAllInspect();
+    }
+}
