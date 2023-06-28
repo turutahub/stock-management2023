3c450ece1fbcd0e80f74a52464b4dcca5910773d
tsurutahub <tsururu421@gmail.com>
2023-06-28 16:33:32 +0900

    ホーム・ダッシュボード

--- ui/home/home.js
+++ ui/home/home.js
@@ -86,57 +86,38 @@ window.onload = function() {
   showContent('home');
 };
 
-// 在庫概要の表示
-function showStockSummary() {
-  // 総在庫数の取得と表示
-  const totalStockElement = document.getElementById('totalStock');
-  const totalStock = getTotalStockFromDatabase(); // データベースから総在庫数を取得
-  totalStockElement.textContent = totalStock;
+function showContent(contentId) {
+  const contents = [
+    'home',
+    'dashboard',
+    'stock',
+    'register',
+    'editForm',
+    'order',
+    'newOrder',
+    'inspect',
+    'stockManagement',
+    'finditem',
+    'reports',
+    'addreport',
+    'settings'
+  ];
 
-  // カテゴリごとの在庫数の取得と表示
-  const categoryStockElement = document.getElementById('categoryStock');
-  const categoryStockData = getCategoryStockFromDatabase(); // データベースからカテゴリごとの在庫数を取得
-  categoryStockElement.innerHTML = '';
-  for (const category in categoryStockData) {
-    const stock = categoryStockData[category];
-    const categoryStockItem = document.createElement('p');
-    categoryStockItem.textContent = `${category}: ${stock}`;
-    categoryStockElement.appendChild(categoryStockItem);
-  }
-
-// 重要なアラートの表示
-function showImportantAlerts() {
-  const importantAlertsElement = document.getElementById('importantAlerts');
-  const importantAlerts = getImportantAlertsFromDatabase(); // データベースから重要なアラートを取得
-  importantAlertsElement.innerHTML = '';
-  for (const alert of importantAlerts) {
-    const alertItem = document.createElement('li');
-    alertItem.textContent = alert;
-    importantAlertsElement.appendChild(alertItem);
-  }
+  contents.forEach(content => {
+    const contentElement = document.getElementById(content);
+    if (contentElement) {
+      contentElement.style.display = contentId === content ? 'block' : 'none';
+    }
+  });
 }
+// 初期表示時にホームと時計を表示
+window.onload = function() {
+  showContent('home');
+};
 
-// 最新の発注情報
-function showLatestShipments() {
-  const latestShipmentsElement = document.getElementById('latestShipments');
-  const latestShipments = getLatestShipmentsFromDatabase(); // データベースから最新の発注情報を取得
-  latestShipmentsElement.innerHTML = '';
-  for (const shipment of latestShipments) {
-    const shipmentItem = document.createElement('li');
-    shipmentItem.textContent = shipment;
-    latestShipmentsElement.appendChild(shipmentItem);
-  }
-}
-
-// 在庫概要、重要なアラート、最新の出荷情報の表示を更新
-function updateDashboard() {
-  showStockSummary();
-  showImportantAlerts();
-  showLatestShipments();
-}
-
-// ページ読み込み時にダッシュボードを更新
-window.addEventListener('load', updateDashboard);
+function openSettings() {
+  // 設定画面に遷移する処理
+  window.location.href = 'settings.html';
 }
 
 
